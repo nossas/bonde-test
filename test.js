0595f00b180ec4c20393b59c4b907e5308492c64
Lucas Pirola <lucaspirola@gmail.com>
2017-10-21 15:09:45 -0200

    feature(devops): Install puppeteer as api to devtools from chrome headless

--- test.js
+++ test.js
@@ -1,10 +1,31 @@
 import test from 'ava';
+import puppeteer from'puppeteer';
 
 test('foo', t => {
   t.pass();
 });
 
 test('bar', async t => {
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://google.com', {waitUntil: 'networkidle'});
+  // Type our query into the search bar
+  await page.type('input[name=q]', 'puppeteer');
+
+  await page.click('input[type="submit"]');
+
+  // Wait for the results to show up
+  await page.waitForSelector('h3 a');
+
+  // Extract the results from the page
+  const links = await page.evaluate(() => {
+    const anchors = Array.from(document.querySelectorAll('h3 a'));
+    return anchors.map(anchor => anchor.textContent);
+  });
+  console.log(links.join('\n'));
+  await browser.close();
+
   const bar = Promise.resolve('bar');
 
   t.is(await bar, 'bar');
