9cce01c3d8418d2c73804ade80600cc65d464d42
Lucas Pirola <lucaspirola@gmail.com>
2017-10-24 03:17:20 -0200

    feature(account): Add tests to logou and edit account

--- test.js
+++ test.js
@@ -1,31 +1,28 @@
 import test from 'ava';
 import puppeteer from'puppeteer';
 
-// test.before(async t => {
-// });
-
 test.beforeEach(async t => {
   const browserConfig = {
-    // executablePath: 'google-chrome-unstable',
+    executablePath: 'google-chrome-unstable',
     // headless: false,
     args: ['--no-sandbox']
   }
   const browser = await puppeteer.launch(browserConfig)
+  // const browserWSEndpoint = browser.wsEndpoint()
   const page = await browser.newPage();
-
-  t.context = { browser, page };
-
+  const mouse = page.mouse
+  t.context = { browser, page, mouse };
   await page.setViewport({ width: 1024, height: 768 })
-  await page.goto('https://app.staging.bonde.org/login', {waitUntil: 'networkidle'});
+  await page.goto('https://app.staging.bonde.org/login');
 
   await page.waitForSelector('input[id="emailId"]');
-  await page.type('input[id="emailId"]', 'foo@bar.com', {delay: 100});
-  await page.type('input[id="passwordId"]', 'foobar', {delay: 150});
-  await page.click('button[type="submit"] span', {delay: 400});
+  await page.type('input[id="emailId"]', 'foo@bar.com');
+  await page.type('input[id="passwordId"]', 'foobar');
   await page.screenshot({path: 'screenshots/0-login.png'});
+  await page.click('button[type="submit"] span');
 
   await page.waitForSelector('.rounded.bg-white .ListItem');
-  await page.screenshot({path: 'screenshots/1-communities-list.png'});
+  await page.screenshot({path: 'screenshots/01-communities-list.png'});
   await page.click('.rounded.bg-white .ListItem:first-child');
 
   await page.waitForSelector('.settings-page-menu-layout');
@@ -33,39 +30,33 @@ test.beforeEach(async t => {
 
 test('admin - mobilization list - /mobilizations', async t => {
   const { browser, page } = t.context;
-  await page.screenshot({path: 'screenshots/2-mobilizations-list.png'});
+  await page.screenshot({path: 'screenshots/02-mobilizations-list.png'});
   const title = await page.$eval('.settings-page-menu-layout h1', e => e.textContent)
   t.is(title, 'Suas Mobilizações');
-  await browser.close();
 });
 
 test('admin - community - change', async t => {
-  const { browser, page } = t.context;
-
-  const mouse = page.mouse
-  mouse.move(30, 30)
+  const { browser, page, mouse } = t.context;
+  mouse.move(1, 1)
   await page.click('.sidenav .item-community-change a.col-4 span');
 
   await page.waitForSelector('.rounded.bg-white .ListItem');
-  await page.screenshot({path: 'screenshots/3-communities-list.png'});
+  await page.screenshot({path: 'screenshots/03-communities-list.png'});
   const title = await page.$eval('.content h2 span', e => e.textContent)
   await page.click('.rounded.bg-white .ListItem:first-child');
   t.is(title, 'Escolha uma das suas comunidades');
-  await browser.close();
 })
 
 test('admin - community - config info', async t => {
-  const { browser, page } = t.context;
+  const { browser, page, mouse } = t.context;
 
-  const mouse = page.mouse
-  mouse.move(10, 10)
-  await page.click('.sidenav .item-community-change a.col-8 span');
-  mouse.move(300, 300)
+  mouse.move(2, 2)
+  await page.click('.sidenav .item-community-change a.col-8 span', { delay: 100 });
+  mouse.move(3, 3)
   await page.waitForSelector('.form-redux.form.transparent');
-  await page.screenshot({path: 'screenshots/4-community-config.png'});
+  await page.screenshot({path: 'screenshots/04-community-config.png'});
   const title = await page.$eval('.settings-page-menu-layout h1', e => e.textContent)
   t.is(title, 'Configurações da comunidade');
-  await browser.close();
 })
 
 test.todo('admin - community - config invite')
@@ -73,7 +64,16 @@ test.todo('admin - community - config mailchimp')
 test.todo('admin - community - config twillio')
 test.todo('admin - community - config recipient')
 test.todo('admin - community - config report')
-test.todo('admin - community - config domain')
+
+test('admin - community - config domain', async t => {
+  const { browser, page, mouse } = t.context
+  await page.click('.sidenav .items:nth-child(2) .item:nth-child(4) .item-icon', { delay: 100 })
+  await page.waitForSelector('.domain-page')
+  const title = await page.$eval('.tab.is-active', e => e.textContent)
+  t.is(title, 'Domínios')
+  mouse.move(200, 200)
+  await page.screenshot({path: 'screenshots/30-account-edit.png'})
+})
 
 test.todo('admin - mobilization - create from template master - /mobilizations/new')
 test.todo('admin - mobilization - add content')
@@ -98,5 +98,28 @@ test.todo('admin - mobilization - template create - /mobilizations/:id/templates
 test.todo('admin - mobilization - create mobilization from template - /mobilizations/new')
 test.todo('admin - mobilization - template remove ')
 
-test.todo('admin - account - my account')
-test.todo('admin - account - logout')
+test('admin - account - my account', async t => {
+  const { browser, page, mouse } = t.context;
+  mouse.move(4, 4)
+  await page.click('.sidenav .items:nth-child(3) .item:nth-child(1) .item-icon', { delay: 100 });
+  await page.waitForSelector('.settings-page-menu-layout h1');
+  const title = await page.$eval('.settings-page-menu-layout h1', e => e.textContent)
+  t.is(title, 'Minha conta');
+  mouse.move(200, 200)
+  await page.screenshot({path: 'screenshots/30-account-edit.png'});
+})
+
+test('admin - account - logout', async t => {
+  const { browser, page, mouse } = t.context;
+  mouse.move(-5, -5)
+  await page.click('.sidenav .items:nth-child(3) .item:nth-child(2) .item-icon', { delay: 100 });
+  await page.waitForSelector('img[alt="Logo Bonde"]')
+  const title = await page.$eval('button[type="submit"] span', e => e.textContent)
+  t.is(title, 'Entrar');
+  await page.screenshot({path: 'screenshots/31-account-logout.png'})
+})
+
+test.afterEach(async t => {
+  const { browser } = t.context;
+  await browser.close();
+})
